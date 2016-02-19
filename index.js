94bf2e1a9449e8a33e34745c20603d5bcfb41fb2
Nosthertus <darkfreak17@hotmail.com>
2016-02-19 01:24:30 -0430

    Added: logic code generator, Modified: raml parser dir
    *Added code to generate routes for each resources in different file resources
    *ramlParser dir has been moved to lib

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-var raml = require('./bin/raml.js');
-var file = require('./bin/file.js');
+var raml = require('./lib/raml.js');
+var fs = require('fs');
+var utils = require('utils')._;
+
+var ramlParser = new raml('test/api.raml');
+
+var resources = ramlParser.routes();
+
+utils.each(resources, function(route){
+	var text = '',
+	tab = "\t",
+	methods = ramlParser.methods(route),
+	routes = [];
+
+	utils.each(methods, function(method){
+		t = '';
+		t += route + '.' + method + "(function($){\n";
+		t += tab + "//your code here\n";
+		t += "});";
+
+		routes.push(t);
+	});
+
+	text = routes.join("\n\n");
+
+	fs.mkdir('test/routes', function(){
+		fs.writeFile('test/routes/' + route + '.js', text, function(err){
+			if(err)
+				throw err;
+
+			console.log('file:', route, 'successfully written');
+		});
+	});
+
+});
