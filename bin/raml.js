993dbd1b9a263ed998d1090044e9789df3a108cb
Nosthertus <darkfreak17@hotmail.com>
2016-02-18 23:50:11 -0430

    Added: object methods in ramlParser
    *New methods added for ramlParser
        -routes: gets the list of all root resources
        -resource: gets the resource object with route name provided
        -methods: gets the list of all methods within the given route name

--- bin/raml.js
+++ bin/raml.js
@@ -1,1 +1,70 @@
 var raml = require('raml-1-parser');
+var path = require('path');
+
+/**
+ * Instantiate object contstruct
+ * @param  {String} file The directory of the raml file
+ */
+var ramlParser = function(file){
+	this._file = file;
+
+	this.api = raml.loadApiSync(path.resolve(__dirname, '../', this._file));
+};
+
+/**
+ * Get all routes from the root
+ * this means that all child resources are ignored
+ * 
+ * @return {Array} The list of all root resources
+ */
+ramlParser.prototype.routes = function () {
+	var resources = [];
+
+	var res = this.api.resources();
+
+	for(i in res){
+		var r = res[i].absoluteUri();
+
+		resources.push(r.split('/')[1]);
+	}
+
+	return resources;
+};
+
+/**
+ * Get a single resource with the provided route name
+ * 
+ * @param  {String} name The name of the route
+ * @return {Resource}      The resource object instance
+ */
+ramlParser.prototype.resource = function(name) {
+	var res = this.api.resources();
+
+	for(i in res){
+		var r = res[i].absoluteUri();
+		var n = r.split('/')[1];
+
+		if(n == name)
+			return res[i];
+	}
+};
+
+/**
+ * Get all methods inside a given route name
+ * 
+ * @param  {String} routeName The name of the route
+ * @return {Array}            The list of all methods in a route
+ */
+ramlParser.prototype.methods = function(routeName) {
+	var m = this.resource(routeName).methods();
+
+	var methods = [];
+
+	for(i in m){
+		methods.push(m[i].method());
+	}
+
+	return methods;
+};
+
+module.exports = ramlParser;
